4ad33d75a4f5dd00f6e746ac86cb3ecf2ebe4d63
Jhon Herrera <jhon.h@idhealth.co>
2024-01-20 16:57:28 -0500

    InvalidateQuery

--- src/users/views/HomeView.tsx
+++ src/users/views/HomeView.tsx
@@ -1,6 +1,11 @@
 import { UserList } from "../components"
 
+import { useInvalidateOnVisibilityChange } from "../../hooks/useInvalidateOnVisibleChange";
+
+
 export const HomeView = () => {
+    useInvalidateOnVisibilityChange();
+
     return (
         <div>
             <div>
